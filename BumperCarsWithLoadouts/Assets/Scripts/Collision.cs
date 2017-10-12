d5a4549be348a69df58afa5b1ef0636b2be7c073
MAIN\mcl1601 <mcl1601@rit.edu>
2017-10-12 15:36:07 -0400

    More work with collisios

--- BumperCarsWithLoadouts/Assets/Scripts/Collision.cs
+++ BumperCarsWithLoadouts/Assets/Scripts/Collision.cs
@@ -7,32 +7,39 @@ public class Collision : MonoBehaviour
     public float impactForce;
 
     private Rigidbody rb;
+    private Player p;
+
+    private int collisionCount;
 	// Use this for initialization
 	void Start ()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        p = gameObject.GetComponent<Player>();
+        collisionCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        collisionCount = 0;
 	}
 
     public void OnCollisionEnter(UnityEngine.Collision collision)
     {
-        if (collision.gameObject.tag == "Terrain")
+        if (collision.gameObject.tag == "Terrain" ||collisionCount > 0)
             return;
 
         Vector3 between = Vector3.Normalize(collision.transform.position - transform.position);
         float vProj = Vector3.Dot(rb.velocity, between);
 
-        Vector3 force = (vProj * between) * impactForce;
+        Vector3 force = vProj * rb.velocity * impactForce;
 
-        rb.AddForce(-force);
-        collision.gameObject.GetComponent<Rigidbody>().AddForce(force);
+        p.ApplyForce(-force);
+        collision.gameObject.GetComponent<Player>().ApplyForce(force);
         collision.transform.forward = Vector3.Lerp(collision.transform.forward, collision.transform.forward + force, Time.deltaTime);
 
-        Debug.Log("Collision with force " + force);
+        Debug.Log("Collision");
+
+        collisionCount++;
     }
 }
