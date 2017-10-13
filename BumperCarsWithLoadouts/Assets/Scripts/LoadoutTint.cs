23f1fa25d8041ccf9c2f7b289b8d84ea3cad3f4a
MAIN\mxo7550 <mxo7550@rit.edu>
2017-10-13 09:59:59 -0400

    Added framework through comments for tinting

---
+++ BumperCarsWithLoadouts/Assets/Scripts/LoadoutTint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadoutTint : MonoBehaviour {
+
+	// Use this for initialization
+    //Create left and right buttons that will change tint
+    
+    //Create array of colors
+	void Start ()
+    {
+	    //Assign values in array
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//if the left button is clicked
+            //decrease the enum value 
+        //if the right button is clicked
+            //increase the enum value
+        //Assign the material to the corresponding enum value
+	}
+}
