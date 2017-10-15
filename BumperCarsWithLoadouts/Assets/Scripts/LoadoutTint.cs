a65f9eb3925278684b0f9af031c770b4f94a01fb
MAIN\mxo7550 <mxo7550@rit.edu>
2017-10-15 16:54:02 -0400

    Finished(?) prototype tint script

--- BumperCarsWithLoadouts/Assets/Scripts/LoadoutTint.cs
+++ BumperCarsWithLoadouts/Assets/Scripts/LoadoutTint.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class LoadoutTint : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
     //Create left and right buttons that will change tint
-    
+    public Dropdown dropDownOne;
+    public Dropdown dropDownTwo;
     //Create array of colors
 	void Start ()
     {
@@ -17,10 +18,41 @@ public class LoadoutTint : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		//if the left button is clicked
-            //decrease the enum value 
+        //if the left button is clicked
+        //decrease the enum value 
         //if the right button is clicked
-            //increase the enum value
+        //increase the enum value
         //Assign the material to the corresponding enum value
-	}
+        switch(dropDownOne.captionText.text)
+        {
+            case "Red":
+                GameObject.Find("PlayerCar").GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case "Blue":
+                GameObject.Find("PlayerCar").GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case "Green":
+                GameObject.Find("PlayerCar").GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case "Yellow":
+                GameObject.Find("PlayerCar").GetComponent<Renderer>().material.color = Color.yellow;
+                break;
+        }
+        switch (dropDownTwo.captionText.text)
+        {
+            case "Red":
+                GameObject.Find("PlayerCar 2").GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case "Blue":
+                GameObject.Find("PlayerCar 2").GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case "Green":
+                GameObject.Find("PlayerCar 2").GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case "Yellow":
+                GameObject.Find("PlayerCar 2").GetComponent<Renderer>().material.color = Color.yellow;
+                break;
+        }
+
+    }
 }
