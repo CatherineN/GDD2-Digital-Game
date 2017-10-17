c43abffd9ce0ec3f18c1dc24f064aed94f4f6426
mxo7550 <mxo7550@IGM2000-02>
2017-10-17 17:18:25 -0400

    Add cannon and cannonball

---
+++ BumperCarsWithLoadouts/Assets/Scripts/Loadouts/Cannonball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonball : MonoBehaviour
+{
+    //variables for speed and velocity
+    private float speed;
+    private Vector3 velocity;
+    public Vector3 direction;
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        velocity = direction * speed;
+        transform.position += velocity;
+	}
+}
