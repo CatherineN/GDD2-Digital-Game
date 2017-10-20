3fcba219c963a14deb2e2ffe8a359e7c8c2a86fb
kxw4589 <kxw4589@IGM2350-19>
2017-10-19 20:17:38 -0400

    switching camera views for P2 is now keypad 1 instead of keypad 0 (keypad 0 is now ability), adjusted input manager and loadout scripts so only the corresponding car will activate their ability when the correct key is pressed

--- BumperCarsWithLoadouts/Assets/Scripts/Loadouts/Cannon.cs
+++ BumperCarsWithLoadouts/Assets/Scripts/Loadouts/Cannon.cs
@@ -6,23 +6,38 @@ public class Cannon : MonoBehaviour {
 
     // Use this for initialization
     public GameObject cannonball;
-	void Start ()
+    private int playerID;
+    void Start ()
     {
-        //Debug.LogWarning(gameObject.GetComponent<Renderer>().material.color);
         gameObject.GetComponent<Renderer>().material.color = transform.GetComponentInParent<Renderer>().material.color;
-        //Debug.LogWarning(gameObject.GetComponentInChildren<MeshRenderer>().gameObject);
+        //set the player id
+        playerID = GetComponentInParent<Player>().playerID;
     }
 
     // Update is called once per frame
     void Update ()
     {
         gameObject.GetComponent<Renderer>().material.color = transform.parent.GetComponentInParent<Renderer>().material.color;
-        //When the E key is pressed...
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Fire3"))
+        switch (playerID)
         {
-            //Create a cannonball at the mouth of the cannon. The cannon is rotated, so we use the transform.up
-            GameObject cannonballInstance = Instantiate(cannonball, gameObject.transform.position + transform.up, new Quaternion(0,0,0,0));
-            cannonballInstance.GetComponent<Cannonball>().direction = transform.up;
+            case 1:
+                //When the E key is pressed...
+                if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Fire3"))
+                {
+                    //Create a cannonball at the mouth of the cannon. The cannon is rotated, so we use the transform.up
+                    GameObject cannonballInstance = Instantiate(cannonball, gameObject.transform.position + transform.up, new Quaternion(0, 0, 0, 0));
+                    cannonballInstance.GetComponent<Cannonball>().direction = transform.up;
+                }
+                break;
+            case 2:
+                //When the E key is pressed...
+                if (Input.GetKeyDown(KeyCode.Keypad0) || Input.GetButtonDown("Fire2"))
+                {
+                    //Create a cannonball at the mouth of the cannon. The cannon is rotated, so we use the transform.up
+                    GameObject cannonballInstance = Instantiate(cannonball, gameObject.transform.position + transform.up, new Quaternion(0, 0, 0, 0));
+                    cannonballInstance.GetComponent<Cannonball>().direction = transform.up;
+                }
+                break;
         }
     }
 }
