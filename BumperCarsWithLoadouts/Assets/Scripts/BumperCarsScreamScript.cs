dc1cdc340f73301b4ed3905981ddc342e20ec8f2
big sexy <no>
2017-10-22 18:28:03 -0400

    start screen has screams

---
+++ BumperCarsWithLoadouts/Assets/Scripts/BumperCarsScreamScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperCarsScreamScript : MonoBehaviour {
+    public AudioClip[] screams;
+    public AudioClip currentScream;
+	// Use this for initialization
+	void Start () {
+        currentScream = screams[Random.Range(0, screams.Length - 1)];
+        gameObject.GetComponent<AudioSource>().PlayOneShot(currentScream);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
