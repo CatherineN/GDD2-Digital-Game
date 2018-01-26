32ad0baa1930b1f5f4fa6b859e6ce11d51c02a2f
MAIN\mcl1601 <mcl1601@rit.edu>
2018-01-25 21:09:46 -0500

    Added in color slider to alter car color, reimported bumper car so it doesn't have transformations, car color saves between scenes

--- BumperCarsWithLoadouts/Assets/Scripts/Loadouts/LoadoutTint.cs
+++ BumperCarsWithLoadouts/Assets/Scripts/Loadouts/LoadoutTint.cs
@@ -7,24 +7,36 @@ public class LoadoutTint : MonoBehaviour {
 
     // Use this for initialization
     //Create left and right buttons that will change tint
-    public Dropdown dropDownOne;
-    public Dropdown dropDownTwo;
+    /*public Dropdown dropDownOne;
+    public Dropdown dropDownTwo;*/
 
-    public Texture2D red;
+    /*public Texture2D red;
     public Texture2D yellow;
     public Texture2D green;
-    public Texture2D blue;
+    public Texture2D blue;*/
+
+    public Slider p1r;
+    public Slider p1g;
+    public Slider p1b;
+    public Slider p2r;
+    public Slider p2g;
+    public Slider p2b;
+
+    public Material p1Color;
+    public Material p2Color;
 
     void Start ()
     {
-	}
+        p1Color.color = Color.white;
+        p2Color.color = Color.white;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         //Assign the material to the corresponding value
         // save the Material of each object and store it in a value -- this will update every frame
-        Material p1Material = GameObject.FindGameObjectWithTag("P1").GetComponent<Renderer>().material;
+        /*Material p1Material = GameObject.FindGameObjectWithTag("P1").GetComponent<Renderer>().material;
         Material p2Material = GameObject.FindGameObjectWithTag("P2").GetComponent<Renderer>().material;
 
         switch (dropDownOne.captionText.text)
@@ -65,6 +77,16 @@ public class LoadoutTint : MonoBehaviour {
                 p2Material.mainTexture = yellow;
                 PlayerPrefs.SetString("color2", "yellow");
                 break;
-        }
+        }*/
+    }
+
+    public void P1ColorChanged()
+    {
+        p1Color.color = new Color(p1r.value, p1g.value, p1b.value);
+    }
+
+    public void P2ColorChanged()
+    {
+        p2Color.color = new Color(p2r.value, p2g.value, p2b.value);
     }
 }
