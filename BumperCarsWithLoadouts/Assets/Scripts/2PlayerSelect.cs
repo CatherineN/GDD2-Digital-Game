8e8d89c5f75cb56e22605caa16de0177562b7fab
ProfessionaleGameMaeker <mms1944@g.rit.edu>
2018-01-26 20:03:51 -0500

    on the verge of a breakthrough

--- BumperCarsWithLoadouts/Assets/Scripts/2PlayerSelect.cs
+++ BumperCarsWithLoadouts/Assets/Scripts/2PlayerSelect.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour {
 
     public Canvas menuSelect;
+    private bool p1Active = true;
+    private List<Selectable> elements;
+    private Selectable currentActiveElement;
 
 	// Use this for initialization
 	void Start () {
-		
+        elements = Selectable.allSelectables;
+        foreach (Selectable s in elements)
+        {
+            s.interactable = false;
+        }
+        elements[0].interactable = true;
 	}
 	
 	// Update is called once per frame
