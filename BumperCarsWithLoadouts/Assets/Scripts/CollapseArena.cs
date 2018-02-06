ce9a759d44e9079bd930699b2ecd6b01fbf1c492
MAIN\mxo7550 <mxo7550@rit.edu>
2018-02-06 17:17:56 -0500

    Created Collapsing Arena (no colliders yet)

---
+++ BumperCarsWithLoadouts/Assets/Scripts/CollapseArena.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapseArena : MonoBehaviour {
+
+    // Use this for initialization
+    System.Random rand;
+    int randomInt;
+	void Start ()
+    {
+        rand = new System.Random();
+        StartCoroutine(Fall());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+    
+    IEnumerator Fall()
+    {
+        randomInt = rand.Next(0, transform.childCount);
+        transform.GetChild(randomInt).GetComponent<Renderer>().material.color = Color.red;
+        yield return new WaitForSeconds(2);
+        transform.GetChild(randomInt).GetComponent<Rigidbody>().useGravity = true;
+        yield return new WaitForSeconds(2);
+        StartCoroutine(Fall());
+    }
+}
