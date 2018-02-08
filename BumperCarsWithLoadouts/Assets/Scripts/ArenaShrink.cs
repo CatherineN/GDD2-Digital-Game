54460d70c2ba8f503b21771e6b1263af387d2e55
kat weis <kxw4589@g.rit.edu>
2018-02-08 15:25:21 -0500

    Finished default and ice arena countdowns and arena overview

--- BumperCarsWithLoadouts/Assets/Scripts/ArenaShrink.cs
+++ BumperCarsWithLoadouts/Assets/Scripts/ArenaShrink.cs
@@ -20,28 +20,37 @@ public class ArenaShrink : MonoBehaviour {
     public float secsTilShrink = 10;//the delay in seconds until the arena begins shrinking
     public float minSizef = .1f;//the public facing variable that determines how small the arena will end up being
     private float startTime;
+    private bool matchStarted = false;//whether or not the match has started
 
     private void Awake()
     {
         //make sure that the default size of the stage is correct at the start of the match
         transform.localScale = Vector3.one;
-        //make the timer reflect the seconds until the arena shrinks
-        GameObject.Find("topCanvas").GetComponent<Timer>().timeLeft = secsTilShrink;
-        GameObject.Find("bottomCanvas").GetComponent<Timer>().timeLeft = secsTilShrink;
-        //make the timer reflect the curretn status of the stage shrinking
-        GameObject.Find("topCanvas").GetComponent<Timer>().endMessage = "WARNING: Arena Shrinking!";
-        GameObject.Find("bottomCanvas").GetComponent<Timer>().endMessage = "WARNING: Arena Shrinking!";
+        
     }
 
     // Use this for initialization
     void Start () {
         minSize = new Vector3(minSizef, minSizef, minSizef);
         defaultSize = Vector3.one;
-        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(matchStarted == false)
+        {
+            if(GameObject.Find("PlayerCar").GetComponent<Player>().enabled)
+            {
+                SetupTimer();
+                matchStarted = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+
         float timeElasped = Time.time - startTime;
         //if the match has been going for too long, shrink the arena
 		if(timeElasped > secsTilShrink)
@@ -56,5 +65,17 @@ public class ArenaShrink : MonoBehaviour {
     {
         //make sure that the default size of the stage is correct at the end of the match
         defaultSize = transform.localScale = Vector3.one;
+    }
+
+    private void SetupTimer()
+    {
+        //make the timer reflect the seconds until the arena shrinks
+        GameObject.Find("topCanvas").GetComponent<Timer>().timeLeft = secsTilShrink;
+        GameObject.Find("bottomCanvas").GetComponent<Timer>().timeLeft = secsTilShrink;
+        //make the timer reflect the curretn status of the stage shrinking
+        GameObject.Find("topCanvas").GetComponent<Timer>().endMessage = "WARNING: Arena Shrinking!";
+        GameObject.Find("bottomCanvas").GetComponent<Timer>().endMessage = "WARNING: Arena Shrinking!";
+
+        startTime = Time.time;
     }
 }
