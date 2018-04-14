849a9a351288f4f1c3841c6ae986328a40be06ca
MAIN\mcl1601 <mcl1601>
2018-04-14 18:09:25 -0400

    Volcano Arena now has eruptions and falling rocks!

---
+++ BumperCarsWithLoadouts/Assets/Scripts/Arena/Volcano/FallingRock.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingRock : MonoBehaviour
+{
+    public float velocity = 2f;
+    public bool decoration = false;
+    private bool falling;
+	// Use this for initialization
+	void Start ()
+    {
+        falling = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!decoration)
+        {
+            if (transform.position.y > 0.13f)
+            {
+                transform.position += Vector3.down * velocity;
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, 0.13f, transform.position.z);
+                Destroy(this);
+            }
+        }
+		else
+        {
+            if (transform.position.y > -100f)
+            {
+                transform.position += Vector3.down * velocity;
+            }
+            else
+                Destroy(gameObject);
+        }
+	}
+}
---
+++ BumperCarsWithLoadouts/Assets/Scripts/Arena/Volcano/VolcanoManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoManager : MonoBehaviour
+{
+    public float timeBetweenEruptions = 15f;
+    public int rocksPerEruption = 1;
+    public int decoRocksPer = 10;
+    public int maxEruptions = -1;
+    public GameObject rockGO;
+    public GameObject decoRock;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(timer >= timeBetweenEruptions)
+        {
+            Erupt();
+            timer = 0f;
+        }
+
+        timer += Time.deltaTime;
+	}
+
+    private void Erupt()
+    {
+        for(int i = 0; i < rocksPerEruption; i++)
+        {
+            Vector2 rand = Random.insideUnitCircle.normalized;
+            float randF = Random.Range(51.75f, 67.7f);
+            Vector3 randUnit = new Vector3(rand.x * randF, Random.Range(200f, 400f), rand.y * randF);
+            Instantiate(rockGO, randUnit, Quaternion.identity);
+        }
+        for (int i = 0; i < decoRocksPer; i++)
+        {
+            Vector2 rand = Random.insideUnitCircle.normalized;
+            float randF = Random.Range(76f, 100f);
+            Vector3 randUnit = new Vector3(rand.x * randF, Random.Range(200f,400f), rand.y * randF);
+            Instantiate(decoRock, randUnit, Quaternion.identity);
+        }
+        StartCoroutine(EruptionEffect());
+    }
+
+    IEnumerator EruptionEffect()
+    {
+        GameObject p = GameObject.Find("Erupt");
+        ParticleSystem pSystem = p.GetComponent<ParticleSystem>();
+        ParticleSystem.EmissionModule em = pSystem.emission;
+        for (int i = 0; i < 60; i++)
+        {
+            em.enabled = true;
+            yield return null;
+        }
+        em.enabled = false;
+    }
+}
