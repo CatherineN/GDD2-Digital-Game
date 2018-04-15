c2b1c26189bec66cb888271652cccaa8b833b739
Kat Weis <kxw4589@g.rit.edu>
2018-04-15 19:52:43 -0400

    I think this actually works for pushing the reset timer over to physics

--- BumperCarsWithLoadouts/Assets/Scripts/Game UI/Timer.cs
+++ BumperCarsWithLoadouts/Assets/Scripts/Game UI/Timer.cs
@@ -8,18 +8,18 @@ public class Timer : MonoBehaviour {
     public Text[] timerLabels;//the UI element used to display the timer
     public float timeLeft;//time in the timer in seconds
     public string endMessage;//the string that is displayed once the timer ticks down to 0
+    public string countMsg;//the string that is displayed while the timer ticks down to 0
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    public virtual void Update () {
         timeLeft -= Time.deltaTime;
 
-        int minutes = (int)timeLeft / 60;
-        int seconds = (int)timeLeft % 60;
+        string formattedTime = FormatTime(timeLeft);
 
         //update the label
         foreach (Text label in timerLabels)
@@ -28,20 +28,36 @@ public class Timer : MonoBehaviour {
             {
                 label.text = endMessage;
             }
-            else if(minutes > 0)//else display the time remaining properly formatted
-            {
-                label.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-            }
-            else if(seconds >= 10)
-            {
-                label.text = string.Format("{0:00}", seconds);
-            }
             else
             {
-                label.text = string.Format("{0:0}", seconds);
+                label.text = countMsg + formattedTime;
             }
         }
 
         
+    }
+
+    /// <summary>
+    /// Formats a float into a formatted string in minutes and seconds using stardard format
+    /// </summary>
+    /// <param name="time">The time that needs to be formatted into minutes and seconds</param>
+    /// <returns>Time in the format of min:secs</returns>
+    protected string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time % 60;
+
+        if (minutes > 0)//else display the time remaining properly formatted
+        {
+            return string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else if (seconds >= 10)
+        {
+            return string.Format("{0:00}", seconds);
+        }
+        else
+        {
+            return string.Format("{0:0}", seconds);
+        }
     }
 }
